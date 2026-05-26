ccc12d42db50a46ec855f4b4c9cea3066b97eabb
Yashodhan Rajapkar <yashraj9769@gmail.com>
2026-05-26 14:45:00 +0530

    Wire up Application class and MainActivity

---
+++ app/src/main/java/com/opsec/space/MainActivity.kt
@@ -0,0 +1,32 @@
+﻿package com.opsec.space
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.opsec.space.ui.navigation.OpsecNavGraph
+import com.opsec.space.ui.theme.OpsecTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            OpsecTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    OpsecNavGraph()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/opsec/space/OpsecApplication.kt
@@ -0,0 +1,19 @@
+﻿package com.opsec.space
+
+import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.decode.VideoFrameDecoder
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class OpsecApplication : Application(), ImageLoaderFactory {
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .components {
+                add(VideoFrameDecoder.Factory())
+            }
+            .crossfade(true)
+            .build()
+    }
+}
